67f91c1d87fc788a675c25e55bb68d9e3fa4b666
JRLM <jrlm@JRLMs-Air.lan>
2017-02-08 19:02:02 -0800

    Krawkutook

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
+from kivy.vector import Vector
+from kivy.clock import Clock
+
 class PongGame(Widget):
-    pass
+   ball=ObjectProperty(None)
+
+
+
+   def update(self, dt):
+       self.ball.move()
+       if(self.ball.y < 0) or (self.ball.top > self.hight):
+           self.ball.velocity_y *= -1
+        pass
 
 class PongApp(App):
     def build(self):
-        return PongGame()
+       game = PongGame()
+       Clock.schedule_interval(game.update, 1.0/60.0)
+    return game
+
+class PongBall(Widget):
+
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+    
+    velocity = ReferenceListProperty(velocity_x,velocity_y)
+
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
 
 if __name__ == '__main__':
     PongApp().run()
+
+
+
+    
